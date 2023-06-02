ee17d2c9577efeaf50c6ecd7e0d3409a2ee93929
your_name <mak30597@gmail.com>
2023-06-02 10:42:25 +0500

    database

--- server/database/OnlineStudentPortal.sql
+++ server/database/OnlineStudentPortal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 17, 2023 at 07:04 AM
+-- Generation Time: Jun 02, 2023 at 07:41 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.28
 
@@ -136,12 +136,14 @@ CREATE TABLE `student` (
   `user_id` int(11) DEFAULT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
   `date_of_birth` date NOT NULL,
   `gender` varchar(255) NOT NULL,
   `phone_number` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `email_address` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
+  `cnic` varchar(255) NOT NULL,
+  `religion` varchar(25) NOT NULL,
   `city` varchar(255) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
   `zip_code` varchar(255) DEFAULT NULL,
@@ -170,17 +172,11 @@ CREATE TABLE `student` (
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`student_id`, `user_id`, `first_name`, `last_name`, `date_of_birth`, `gender`, `phone_number`, `image`, `email_address`, `address`, `city`, `state`, `zip_code`, `country`, `status`, `created_at`, `updated_at`, `created_by`, `updated_by`, `relegion`, `mother_name`, `father_name`, `father_contact`, `father_occupation`, `passport_number`, `family_member`, `t_address`, `land_line`, `t_city`, `t_state`, `t_zip_code`, `t_country`) VALUES
-(4, NULL, 'Muhammad', 'Khan', '2023-05-06', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-02 10:53:37', '2023-05-02 10:53:37', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
-(5, NULL, 'Muhammad', 'Khan', '2023-05-06', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-02 10:57:21', '2023-05-02 10:57:21', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
-(6, NULL, 'Muhammad', 'Khan', '2023-05-06', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-02 11:03:37', '2023-05-02 11:03:37', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
-(7, NULL, 'Muhammad', 'Khan', '2023-05-09', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-02 11:05:35', '2023-05-02 11:05:35', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
-(8, NULL, 'Theodore', 'Brooks', '1972-03-17', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-05 11:56:18', '2023-05-05 11:56:18', NULL, NULL, NULL, 'Deacon Cardenas', 'Rudyard', '+1 (626) 194-7829', 'Buckley', NULL, NULL, NULL, '+1 (101) 254-2507', NULL, NULL, NULL, NULL),
-(9, NULL, 'Francesca', 'Hale', '2019-12-25', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-05 12:05:33', '2023-05-05 12:05:33', NULL, NULL, NULL, 'Heidi Pickett', 'Shelby', '+1 (474) 322-9675', 'Alexander', NULL, NULL, NULL, '+1 (418) 518-3238', NULL, NULL, NULL, NULL),
-(10, NULL, 'Muhammad', 'Khan', '2023-05-11', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-05 12:08:14', '2023-05-05 12:08:14', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
-(11, NULL, 'Muhammad', 'Khan', '2023-05-02', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-05 12:23:35', '2023-05-05 12:23:35', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
-(12, NULL, 'Muhammad', 'Khan', '2023-05-04', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-05 12:24:43', '2023-05-05 12:24:43', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
-(13, NULL, 'Muhammad', 'Khan', '2023-05-04', 'male', '03155038994', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-05 12:27:40', '2023-05-05 12:27:40', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL);
+INSERT INTO `student` (`student_id`, `user_id`, `first_name`, `last_name`, `middle_name`, `date_of_birth`, `gender`, `phone_number`, `image`, `address`, `cnic`, `religion`, `city`, `state`, `zip_code`, `country`, `status`, `created_at`, `updated_at`, `created_by`, `updated_by`, `relegion`, `mother_name`, `father_name`, `father_contact`, `father_occupation`, `passport_number`, `family_member`, `t_address`, `land_line`, `t_city`, `t_state`, `t_zip_code`, `t_country`) VALUES
+(16, 55, 'Muhammadasas', 'Khan', 'null', '2023-05-02', 'male', '03155038997', '/studentsImages/1685517578.jpg', 'sd', '38201-8723031-7', 'christianity', 'asd', 'sd', 'sd', 'sd', NULL, '2023-05-29 06:37:04', '2023-06-02 05:32:54', NULL, NULL, NULL, 'Abdullahsasa', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
+(21, 62, 'zxczxc', 'zxczc', 'zxczc', '2023-05-19', 'male', '03155038994', '/studentsImages/1685519658.jpg', NULL, '61101-8195831-5', 'hinduism', NULL, NULL, NULL, NULL, NULL, '2023-05-31 07:54:18', '2023-05-31 07:54:18', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038994', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
+(22, 63, 'Muhammadd', 'Khan', 'Abdullah', '2023-05-10', 'male', '03155038994', '/studentsImages/1685521364.jpg', NULL, '61101-8195831-5', 'islam', NULL, NULL, NULL, NULL, NULL, '2023-05-31 08:22:44', '2023-05-31 08:25:56', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '0315503899', 'Khan', NULL, NULL, NULL, '03155038994', NULL, NULL, NULL, NULL),
+(23, 68, 'Muhammad Hamza', 'Asif', 'null', '2023-05-09', 'male', '03155038994', '/studentsImages/1685521947.jpg', 'House 571-C Street 17 Margalla Town islamabad Phase 1', '61101-8195831-5', 'christianity', 'Islamabad Pakistan', 'Islamabad', '44000', 'Pakistan', NULL, '2023-05-31 08:32:27', '2023-06-01 09:39:56', NULL, NULL, NULL, 'Abdullah', 'Muhammad', '03155038997', 'Khan', NULL, NULL, 'House 571-C Street 17 Margalla Town islamabad Phase 1', '03155038997', 'Islamabad Pakistan', 'Islamabad', '44000', 'Pakistan');
 
 -- --------------------------------------------------------
 
@@ -245,7 +241,11 @@ INSERT INTO `user` (`user_id`, `email`, `password`, `role`, `status`, `created_a
 (55, 'mak@gmail.com', '$2y$10$qWuBdks3iH8GXtaZcP7/neU3UecSvjuzKndTetXkbqXpMCyYk.ByW', 'Student', 'Active', '2023-04-26 06:56:29', '2023-04-26 06:56:29', 'mak@gmail.com', NULL),
 (56, 'mak30497@gmail.com', '$2y$10$L6V35OcM2MA5aMVYauxNiuth9LOjKtPUNmKLAJJ3bcadVbXHiqF1W', 'Student', 'Active', '2023-04-28 05:21:44', '2023-04-28 05:21:44', 'mak30497@gmail.com', NULL),
 (57, 'asas@gmail.com', '$2y$10$5lnFO1Xj5wVO49.06uln4eoogIC6anf1Othk4sjjQzEHdoLfaw0Du', 'Student', 'Active', '2023-04-28 05:22:50', '2023-04-28 05:22:50', 'asas@gmail.com', NULL),
-(58, 'mak304957@gmail.comss', '$2y$10$djmMHxqmnRw4PB4miwt3ve/I4mzuyMk1gdGq61B6d.b5FDcUSRbDe', 'Student', 'Active', '2023-04-28 05:23:33', '2023-04-28 05:23:33', 'mak304957@gmail.comss', NULL);
+(58, 'mak304957@gmail.comss', '$2y$10$djmMHxqmnRw4PB4miwt3ve/I4mzuyMk1gdGq61B6d.b5FDcUSRbDe', 'Student', 'Active', '2023-04-28 05:23:33', '2023-04-28 05:23:33', 'mak304957@gmail.comss', NULL),
+(61, 'zxc@gmail.com', '$2y$10$qWm4L9G6NyclY1EoQEQp7e8RWtHL7sB1r93QEEvaaSK3YauZEQ/g.', 'Student', 'Active', '2023-05-31 07:24:12', '2023-05-31 07:24:12', 'zxc@gmail.com', NULL),
+(62, 'aa@gmail.com', '$2y$10$YGkN7h4KRn5E.zjADhh0NO/PNpr6c6WLB93vvO/ApSTDSCLIFs2MK', 'Student', 'Active', '2023-05-31 07:43:34', '2023-05-31 07:43:34', 'aa@gmail.com', NULL),
+(63, 'z@gmail.com', '$2y$10$HeqjrCG8AubUljXvsaCpCupdkZ3NR8Azm6k5MPtKlR9Q.YD7S1CPa', 'Student', 'Active', '2023-05-31 07:58:28', '2023-05-31 07:58:28', 'z@gmail.com', NULL),
+(68, 'll@gmail.com', '$2y$10$1WvdPuJHo3gzwSarXmnZE.Xpif76//IBf11MQPBnREXMWo5E3l5.C', 'Student', 'Active', '2023-05-31 08:30:17', '2023-05-31 08:30:17', 'll@gmail.com', NULL);
 
 -- --------------------------------------------------------
 
@@ -376,7 +376,7 @@ ALTER TABLE `program`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `test`
@@ -394,7 +394,7 @@ ALTER TABLE `test_score`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT for table `voucher`
