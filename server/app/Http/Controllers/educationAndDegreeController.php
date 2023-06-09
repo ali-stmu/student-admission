b913e03f687c28fe9fa58ce3b45404b9d36a3c89
your_name <mak30597@gmail.com>
2023-06-09 18:09:20 +0500

    Degrees save ho rhien han storange me work in progress

--- server/app/Http/Controllers/educationAndDegreeController.php
+++ server/app/Http/Controllers/educationAndDegreeController.php
@@ -25,33 +25,42 @@ class EducationAndDegreeController extends Controller
     }
     public function storeDegreeAndDocument(Request $request)
     {
+        //log::debug($request->input('user_id'));
         $student_id_json = $this->findStudentId($user_id = $request->input('user_id'));
         $studentId = $student_id_json->getData()->student_id;
+        //  log::debug($studentId);
+        // Process the incoming form data
+        $resultStatus = $request->input('resultStatus');
+        $qualification = $request->input('qualification');
+        $boardUniversity = $request->input('boardUniversity');
+        $passingYear = $request->input('passingYear');
+        $totalMarksCGPA = $request->input('totalMarksCGPA');
+        $obtainedMarksCGPA = $request->input('obtainedMarksCGPA');
+        $percentage = $request->input('percentage');
+        $degreeFiles = $request->file('degree');
+        $user_id = $request->input('user_id');
 
-        //$user_id = $request->input('user_id');
-        log::debug($records = $request->input('records'));
+        // Perform validation if needed
 
-        // Process each record
-        foreach ($records as $record) {
-            //$resultStatus = $record['resultStatus'];
-            //$qualification = $record['qualification'];
-            //$boardUniversity = $record['boardUniversity'];
-            //$passingYear = $record['passingYear'];
-            //$totalMarksCGPA = $record['totalMarksCGPA'];
-            //$obtainedMarksCGPA = $record['obtainedMarksCGPA'];
-            //$percentage = $record['percentage'];
-            //log::debug($degree = $record['degree']);
+        // Save the data to the database or perform any desired operations
+        // Example:
+        foreach ($resultStatus as $index => $status) {
+            // Assuming you have an "Education" model
+            $education = new Education();
+            $education->resultStatus = $status;
+            $education->qualification = $qualification[$index];
+            $education->boardUniversity = $boardUniversity[$index];
+            $education->passingYear = $passingYear[$index];
+            $education->totalMarksCGPA = $totalMarksCGPA[$index];
+            $education->obtainedMarksCGPA = $obtainedMarksCGPA[$index];
+            $education->percentage = $percentage[$index];
+            // Save the education record
+            // $education->save();
 
-            if (isset($record['degree']) && $record['degree'] instanceof \Illuminate\Http\UploadedFile) {
-                log::debug("hello from inside if");
-                $degreeFile = $record['degree'];
-                $degreeFileName = $degreeFile->getClientOriginalName();
-                log::debug($degreeFile->storeAs('degrees', $degreeFileName, 'public'));
-                // $educationModel->degree = $degreeFileName;
+            // Store the degree file if available
+            if ($degreeFiles[$index]) {
+                log::debug($degreeFiles[$index]->store('degrees'));
             }
         }
-
-        // Return a response
-        return response()->json(['message' => 'Data received successfully']);
     }
 }
