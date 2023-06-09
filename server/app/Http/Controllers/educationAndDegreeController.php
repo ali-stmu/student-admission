3422506fc4256c8dcb787ae8be84e41c1793caf1
your_name <mak30597@gmail.com>
2023-06-09 15:31:42 +0500

    getting student if from user_id

--- server/app/Http/Controllers/educationAndDegreeController.php
+++ server/app/Http/Controllers/educationAndDegreeController.php
@@ -12,36 +12,46 @@ class EducationAndDegreeController extends Controller
 {
 
 
-
+    public function findStudentId($user_id)
+    {
+        $student = Student::where('user_id', $user_id)->first();
+        if ($student) {
+            $student_id = $student->student_id;
+            //Log::debug('Found student_id: ' . $student_id);
+            return response()->json(['student_id' => $student_id]);
+        } else {
+            Log::debug('Student not found for user_id: ' . $user_id);
+        }
+    }
     public function storeDegreeAndDocument(Request $request)
     {
+        $student_id_json = $this->findStudentId($user_id = $request->input('user_id'));
+        $studentId = $student_id_json->getData()->student_id;
 
+        //$user_id = $request->input('user_id');
+        $records = $request->input('records');
 
-        Log::debug($jsonData = $request->all());
-        $education = new Education();
-        //$education->student_id = $this->findStudent(); // Use the findStudent() method to get the student_id
+        // Process each record
+        foreach ($records as $record) {
+            //$resultStatus = $record['resultStatus'];
+            //$qualification = $record['qualification'];
+            //$boardUniversity = $record['boardUniversity'];
+            //$passingYear = $record['passingYear'];
+            //$totalMarksCGPA = $record['totalMarksCGPA'];
+            //$obtainedMarksCGPA = $record['obtainedMarksCGPA'];
+            //$percentage = $record['percentage'];
+            log::debug($degree = $record['degree']);
 
-        foreach ($jsonData['records'] as $record) {
-
-            $education->qualification = $record['qualification'];
-            $education->institution_name = $record['boardUniversity'];
-            // Set other attributes accordingly
-
-            $education->save();
-
-            if ($record['degree']) {
-                $document = new Document();
-                $document->student_id = $this->findStudent(); // Use the findStudent() method to get the student_id
-                $document->document_name = 'Degree';
-                $document->document_file_name = $record['degree']->getClientOriginalName();
-                // Set other attributes accordingly
-
-                // Move the uploaded file to a desired location
-                $record['degree']->move('path/to/store/files', $document->document_file_name);
-
-                $document->save();
+            // Handle the degree file if it's not empty
+            if (!empty($degree)) {
+                log::debug($degreePath = $degree->store('degrees')); // Assuming you have a 'degrees' directory in your storage
+                // Save the degree path to the database or perform any other required operations
             }
+
+            // Save other data to the database or perform any other required operations
         }
-        return response()->json(['message' => 'Data stored successfully']);
+
+        // Return a response
+        return response()->json(['message' => 'Data received successfully']);
     }
 }
