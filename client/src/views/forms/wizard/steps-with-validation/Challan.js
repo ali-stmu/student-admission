1b1cb625da07b3de49fa2e97c95a44e83d982305
Muhammad Ali <ali@fnatiq.com>
2023-08-23 15:06:29 +0500

    view centered

--- client/src/views/forms/wizard/steps-with-validation/Challan.js
+++ client/src/views/forms/wizard/steps-with-validation/Challan.js
@@ -1,21 +1,7 @@
 import React, { useRef } from "react";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  CustomInput,
-  Row,
-  Col,
-  Table,
-} from "reactstrap";
-import { ArrowLeft, ArrowRight } from "react-feather";
-import { useForm } from "react-hook-form";
-
-// ... (import statements)
+import { Button, Row, Col, Table } from "reactstrap";
 
 const Challan = () => {
   const contentRef = useRef(null);
@@ -25,12 +11,23 @@ const Challan = () => {
     if (input) {
       const pdf = new jsPDF("l", "mm", "a4"); // Landscape mode
 
+      // Determine the dimensions based on the device's screen size
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+      const canvasWidth =
+        screenWidth > screenHeight ? screenWidth : screenHeight;
+      const canvasHeight = canvasWidth * 0.75; // Assuming aspect ratio of 4:3
+
       // Set white background before rendering canvas
       const canvasOptions = {
         backgroundColor: "#FFFFFF", // White background color
       };
 
-      html2canvas(input, canvasOptions).then((canvas) => {
+      html2canvas(input, {
+        ...canvasOptions,
+        width: canvasWidth,
+        height: canvasHeight,
+      }).then((canvas) => {
         const imgData = canvas.toDataURL("image/png");
         const width = pdf.internal.pageSize.getWidth();
         const height = (canvas.height * width) / canvas.width;
@@ -41,8 +38,19 @@ const Challan = () => {
     }
   };
 
+  const tableContainerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "stretch",
+    gap: "10px",
+    overflowX: "auto",
+  };
+
   const tableStyle = {
     backgroundColor: "#FFFFFF", // White background color
+    minWidth: "300px", // Minimum width for the entire table
+    maxWidth: "800px", // Maximum width for the entire table
+    width: "100%", // Set the table width to 100% for responsiveness
   };
 
   const thStyle = {
@@ -53,52 +61,44 @@ const Challan = () => {
   return (
     <div style={{ backgroundColor: "#FFFFFF" }}>
       <div ref={contentRef}>
-        <Form>
-          <Row>
-            <Col md={4}>
-              <Table bordered style={tableStyle}>
-                <thead>
-                  <tr>
-                    <th style={thStyle}>Table 1</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Table content here...</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-            <Col md={4}>
-              <Table bordered style={tableStyle}>
-                <thead>
-                  <tr>
-                    <th style={thStyle}>Table 2</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Table content here...</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-            <Col md={4}>
-              <Table bordered style={tableStyle}>
-                <thead>
-                  <tr>
-                    <th style={thStyle}>Table 3</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Table content here...</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-          </Row>
-        </Form>
+        <div style={tableContainerStyle}>
+          <Table bordered style={tableStyle}>
+            <thead>
+              <tr>
+                <th style={thStyle}>Table 1</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Table content here...</td>
+              </tr>
+            </tbody>
+          </Table>
+          <Table bordered style={tableStyle}>
+            <thead>
+              <tr>
+                <th style={thStyle}>Table 2</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Table content here...</td>
+              </tr>
+            </tbody>
+          </Table>
+          <Table bordered style={tableStyle}>
+            <thead>
+              <tr>
+                <th style={thStyle}>Table 3</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Table content here...</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       </div>
       <Button onClick={generatePDF}>Download Challan</Button>
     </div>
