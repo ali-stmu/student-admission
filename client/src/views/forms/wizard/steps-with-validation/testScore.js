b045bbac506c36bdd6a553b21f8461d36aa19563
Muhammad Ali <ali.mis@stmu.edu.pk>
2023-09-11 16:11:48 +0500

    inserting test score in database wip

--- client/src/views/forms/wizard/steps-with-validation/testScore.js
+++ client/src/views/forms/wizard/steps-with-validation/testScore.js
@@ -15,14 +15,38 @@ import {
 import CreatableSelect from "react-select/creatable";
 
 const TestScore = ({ stepper, type }) => {
-  const { control, handleSubmit, register, errors } = useForm();
+  const { control, handleSubmit, register, errors, watch } = useForm();
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [TempUserid, setTempUserid] = useState(null);
+
   const skipToNextStep = () => {
     stepper.next();
   };
-  const onSubmit = (data) => {
-    // Handle form submission here
-    stepper.next();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    try {
+      const formData = new FormData();
+      formData.append("test_name", data.testName?.value || ""); // Use the value property
+      formData.append("test_score_total", data.totalMarks);
+      formData.append("test_score", data.obtainedMarks);
+      formData.append("test_date", data.testYear);
+      formData.append("attachment", data.attachment[0]);
+      formData.append("user_id", TempUserid);
+
+      const response = await fetch(`${BASE_URL}savetestinfo`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        stepper.next();
+      } else {
+        console.error("API call failed:", response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
   };
 
   const previous = () => {
@@ -30,10 +54,18 @@ const TestScore = ({ stepper, type }) => {
   };
 
   useEffect(() => {
-    // Update the current year state when the component mounts
+    const rolesFromStorage = localStorage.getItem("StudentInfo");
+    const studentInfo = JSON.parse(rolesFromStorage);
+    setTempUserid(studentInfo.user_id);
     setCurrentYear(new Date().getFullYear());
   }, []);
 
+  // Define options for the testName dropdown
+  const testNameOptions = [
+    { value: "Option1", label: "Option 1" },
+    { value: "Option2", label: "Option 2" },
+    // Add more options as needed
+  ];
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormGroup>
@@ -42,16 +74,22 @@ const TestScore = ({ stepper, type }) => {
         </Label>
         <Controller
           name="testName"
+          id="testName"
           control={control}
           defaultValue=""
           render={({ field }) => (
-            <CreatableSelect
-              {...field}
-              isClearable
-              className="react-select"
-              classNamePrefix="select"
-              options={[]} // Your dropdown options should be here
-            />
+            <>
+              <CreatableSelect
+                {...field}
+                isClearable
+                className="react-select"
+                classNamePrefix="select"
+                options={testNameOptions}
+                onChange={(value) => {
+                  console.log(value); // Add this line to check the selected value
+                }}
+              />
+            </>
           )}
         />
       </FormGroup>
@@ -63,6 +101,7 @@ const TestScore = ({ stepper, type }) => {
               type="number"
               name="totalMarks"
               id="totalMarks"
+              min="0"
               placeholder="Enter Total Marks"
               innerRef={register({ required: true, pattern: /^\d+$/ })}
             />
@@ -75,12 +114,13 @@ const TestScore = ({ stepper, type }) => {
               type="number"
               name="obtainedMarks"
               id="obtainedMarks"
+              min="0"
               placeholder="Enter Obtained Marks"
               innerRef={register({
                 required: true,
                 pattern: /^\d+$/,
                 validate: (value) =>
-                  parseInt(value) <= parseInt(data.totalMarks),
+                  parseInt(value) <= parseInt(watch("totalMarks")), // Validate against totalMarks value
               })}
             />
             {errors.obtainedMarks && (
@@ -99,8 +139,8 @@ const TestScore = ({ stepper, type }) => {
           id="testYear"
           placeholder="Test Year"
           innerRef={register({ required: true, pattern: /^\d+$/ })}
-          readOnly // Set the readOnly attribute to make it readonly
-          value={currentYear} // Set the value to the current year
+          readOnly
+          value={currentYear}
         />
       </FormGroup>
       <FormGroup>
