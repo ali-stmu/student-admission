6f66e983ed058115a6ff582bbd2c68d0cd8cfbec
Muhammad Ali <ali.mis@stmu.edu.pk>
2023-10-04 09:45:40 +0500

    config changes

--- client/src/configForStudentPictureServer.js
+++ client/src/configForStudentPictureServer.js
@@ -1,1 +1,2 @@
-export const BASE_URL_OF_SERVER = 'http://localhost:8000';
+export const BASE_URL_OF_SERVER = "http://localhost:8000";
+//export const BASE_URL_OF_SERVER = 'http://124.109.47.58:8002';
