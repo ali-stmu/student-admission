aab48d8165737d4dc100b46297c8e6f5a3bf4943
Muhammad Ali <ali.mis@stmu.edu.pk>
2023-10-06 14:40:08 +0500

    admin response with list of programs

--- server/app/Http/Controllers/AdminApplicationController.php
+++ server/app/Http/Controllers/AdminApplicationController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Admin;
+use App\Models\Program; // Import the Program model
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -10,16 +11,37 @@ class AdminApplicationController extends Controller
 {
     public function feeApplicationReceived(Request $request, $user_id)
     {
-        // You can use the $user_id parameter in your logic to fetch data or perform any actions.
-        // For example, you can retrieve data from the database based on the user_id.
-        
-        // Replace this with your logic:
-        $data = [
-            'user_id' => $user_id,
-            // Add more data as needed.
-        ];
-        log::debug($data);
+        try {
+            // Use Eloquent to retrieve the admin details based on the user_id.
+            $admin = Admin::where('user_id', $user_id)->first();
 
-        return response()->json($data);
+            if ($admin) {
+                // Retrieve all programs where admin.college_id matches program.college_id
+                $programs = Program::where('college_id', $admin->college_id)->get();
+
+                if ($programs->isEmpty()) {
+                    return response()->json(['message' => 'No programs found for this college'], 404);
+                }
+                log::debug($programs);
+
+                // You can access the program data in $programs as needed.
+                // For example, if you want to return the list of programs:
+                $programData = $programs->toArray();
+
+                $data = [
+                    'programs' => $programData, // Add the programs data to the response.
+                    // Add more attributes as needed.
+                ];
+
+                return response()->json($data);
+            } else {
+                // Admin not found for the given user_id.
+                return response()->json(['message' => 'Admin not found'], 404);
+            }
+        } catch (\Exception $e) {
+            // Handle any exceptions that might occur during the database query.
+            Log::error($e->getMessage());
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
     }
 }
